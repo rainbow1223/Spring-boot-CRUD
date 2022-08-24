cb91e3ecdf385328707a025b233a05ddbf3fc067
StrongJack123 <greatwisdom1223@gmail.com>
2022-08-24 21:18:58 +0900

    add properties

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,23 +1,23 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
-    @GetMapping
-    private final StudentService studentService;
 
+    private final StudentService studentService;
+    
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
-
+    @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,13 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> getStudents() {
         return List.of(
